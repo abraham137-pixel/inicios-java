4da5fecc9d7978b3598969ebbfd489d51ce0e894
abrahampixel-137 <mrduffy209@gmail.com>
2022-10-31 15:12:04 -0600

    unolisto

---
+++ variables/src/evaluacion.java
@@ -0,0 +1,6 @@
+public class evaluacion{
+ public static void main(String[]  args) {
+    //elmetodo main ejecuta las tareas de la clase
+    System.out.println("mi primer programa java desde cero");
+    }
+}
