773b7794ba9071dc466b26a7ccbfb5ee1800939c
abrahampixel-137 <mrduffy209@gmail.com>
2022-11-04 12:57:42 -0600

    ya estan subidos

--- variables/src/pedido.java
+++ variables/src/pedido.java
@@ -1,12 +1,12 @@
 public class pedido {
     
     public static void main(String[] args) {
+        double costoArticulo = 30.99;
+        boolean listoEnviar = false;
+        System.out.println(costoArticulo >24?"precio alto:":"precio bajo");
+        System.out.println(listoEnviar?"listo para enviar":"pedido no listo");
         
-        if (costo>24);
-        System.out.print("el $es alto");
-        if(alista evoar)
-        {
             
-        }
+        
     }
 }
