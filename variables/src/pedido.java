f23b782df065ef66f3abe00770417da1d1c43e38
esc_codigo5 <escueladecodigo@test5.com>
2022-11-04 12:03:41 -0600

    avanze

---
+++ variables/src/pedido.java
@@ -0,0 +1,12 @@
+public class pedido {
+    
+    public static void main(String[] args) {
+        
+        if (costo>24);
+        System.out.print("el $es alto");
+        if(alista evoar)
+        {
+            
+        }
+    }
+}
