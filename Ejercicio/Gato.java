911b9dacaaeecfb694fa3f79817b1ff3968760d4
esc_codigo5 <escueladecodigo@test5.com>
2022-11-16 12:17:01 -0600

    juego de gato

---
+++ Ejercicio/Gato.java
@@ -0,0 +1,14 @@
+public class Gato {
+
+    public static void main(String[] args) {
+        char[] gato={
+            "s",""
+        }
+        char p= "i";
+        char g= "-";
+        char s= "";
+    }
+    
+
+
+}
---
+++ Ejercicio/Gato/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+// todo lo de abajo hasta el comentario siguiente de este si es parte del cuerpo del codigo
+        Gato gato = new Gato();
+// la parte de aqui es parte del tablero aun faltan los jugadores y lo demas
+        pivate char p = "i";
+        char s = "";
+        char [][] tablero = {
+            {s,p,s,p,s},
+        };
+
+        for ( int flia = o; fila<tablero.length;col++)
+        System.out.println(tablero[fila][col]);
+
+System.out.println(fila!=2?"------/n:":"/n");
+        {"X",}
+// esto de abajo no es parte del cuerpo olas funciones de momento
+        public static void main() {
+            
+        }
+
+
+
+    }
+}
